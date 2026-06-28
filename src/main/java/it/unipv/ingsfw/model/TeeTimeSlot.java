6a3e31a5f7478265707c4f3b84635c4122c67f89
leahappiah <leah8400@gmail.com>
2026-06-28 13:57:12 +0200

    implementata classe TeeTimeSlot

--- src/main/java/it/unipv/ingsfw/model/TeeTimeSlot.java
+++ src/main/java/it/unipv/ingsfw/model/TeeTimeSlot.java
@@ -1,4 +1,27 @@
 package it.unipv.ingsfw.model;
 
-public class TeeTimeSlot {
-}
+/**
+ * Classe per la visualizzazione dello slot del tee-time orario
+ */
+
+ public class TeeTimeSlot {
+
+    private final String time;
+    private final String status;
+    private final String details;
+
+ public TeeTimeSlot(String time, String status, String details) {
+
+    this.time = time;
+    this.status = status;
+    this.details = details;
+
+ }
+
+ public String getTime() { return time; }
+
+ public String getStatus() { return status; }
+
+ public String getDetails() { return details; }
+
+ }
