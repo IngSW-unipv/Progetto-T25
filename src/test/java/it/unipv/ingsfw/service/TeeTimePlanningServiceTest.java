4cd0e44484685f370d64bdaa43cc69ad7188f155
leahappiah <leah8400@gmail.com>
2026-07-09 20:44:02 +0200

    test per TeeTimePlanningService

--- src/test/java/it/unipv/ingsfw/service/TeeTimePlanningServiceTest.java
+++ src/test/java/it/unipv/ingsfw/service/TeeTimePlanningServiceTest.java
@@ -1,4 +1,36 @@
 package it.unipv.ingsfw.service;
 
+
+import it.unipv.ingsfw.model.TeeTimeSlot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TeeTimePlanningServiceTest {
+
+    private TeeTimePlanningService planningService;
+
+    @BeforeEach
+    public void setUp() {
+        planningService = new TeeTimePlanningService();
+    }
+
+
+    @Test
+    public void testGeneraPlanningGiornaliero_DimensioneEStruttura() throws SQLException {
+
+        LocalDate oggi = LocalDate.now();
+
+        List<TeeTimeSlot> planning = planningService.generaPlanningGiornaliero(oggi);
+
+        assertNotNull(planning, "Il planning non dovrebbe essere null");
+        assertEquals(25, planning.size(), "Il numero totale di slot generati tra le 8 e le 12 deve essere 25");
+        assertEquals("08:00", planning.get(0).getTime(), "Il primo slot orario deve essere alle 08:00");
+
+        assertEquals("12:00", planning.get(planning.size() - 1).getTime(), "L'ultimo slot orario deve essere alle 12:00");
+    }
 }
