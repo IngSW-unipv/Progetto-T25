353259723331716780e73b97a0326daf3862904c
leahappiah <leah8400@gmail.com>
2026-07-09 20:41:37 +0200

    test j unit per la classe pricecalculator

--- src/test/java/it/unipv/ingsfw/service/PriceCalculatorTest.java
+++ src/test/java/it/unipv/ingsfw/service/PriceCalculatorTest.java
@@ -1,4 +1,34 @@
 package it.unipv.ingsfw.service;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class PriceCalculatorTest {
+
+    @Test
+    public void testCalcoloPrezzo9Buche() {
+        int holes = 9;
+        int players = 2;
+        int cart = 0;
+        int golfbag = 0;
+        int trolley = 0;
+        double totale = PriceCalculator.calcolaTotale(holes, players, cart, golfbag, trolley);
+
+        assertEquals(80.0, totale);
+    }
+
+
+    @Test
+    public void testCalcoloPrezzo18BucheConExtra() {
+        int holes = 18;
+        int players = 2;
+        int cart = 1;
+        int golfbag = 1;
+        int trolley = 1;
+
+        double totale = PriceCalculator.calcolaTotale(holes, players, cart, golfbag, trolley);
+        double atteso = (60.0 * 2) + 30.0 + 15.0 + 5.0;
+        assertEquals(atteso, totale);
+    }
 }
