6ff13b492e9b617f9b6e50204c01cfa05632818c
leahappiah <leah8400@gmail.com>
2026-07-09 20:42:58 +0200

    implementati test per la classe ReservationValidator

--- src/test/java/it/unipv/ingsfw/service/ReservationValidatorTest.java
+++ src/test/java/it/unipv/ingsfw/service/ReservationValidatorTest.java
@@ -1,4 +1,59 @@
 package it.unipv.ingsfw.service;
 
+import it.unipv.ingsfw.exceptions.InvalidReservationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReservationValidatorTest {
+
+    private ReservationValidator validator;
+
+
+    @BeforeEach
+    public void setUp() {
+        validator = new ReservationValidator();
+    }
+
+
+    @Test
+    public void testValidaDataOggiEDomani_Valide() {
+        assertDoesNotThrow(() -> validator.validaData(LocalDate.now()));
+        assertDoesNotThrow(() -> validator.validaData(LocalDate.now().plusDays(1)));
+    }
+
+    @Test
+    public void testValidaDataNelPassato_LanciaEccezione() {
+        LocalDate dataPassata = LocalDate.now().minusDays(1);
+
+        InvalidReservationException exception = assertThrows(
+                InvalidReservationException.class,
+                () -> validator.validaData(dataPassata)
+        );
+        assertTrue(exception.getMessage().contains("Impossibile inserire una prenotazione nel passato"));
+    }
+
+    @Test
+    public void testValidaAttrezzaturaCoerente_Valida() {
+        assertDoesNotThrow(() -> validator.validaAttrezzatura(2, 2, 1, 1));
+
+        assertDoesNotThrow(() -> validator.validaAttrezzatura(4, 1, 2, 0));
+    }
+
+    @Test
+    public void testValidaTroppeSacche_LanciaEccezione() {
+        assertThrows(
+                InvalidReservationException.class,
+                () -> validator.validaAttrezzatura(2, 3, 0, 0)
+        );
+    }
+
+    @Test
+    public void testValidaTroppiVeicoli_LanciaEccezione() {
+        assertThrows(
+                InvalidReservationException.class,
+                () -> validator.validaAttrezzatura(2, 1, 2, 1)
+        );
+    }
 }
