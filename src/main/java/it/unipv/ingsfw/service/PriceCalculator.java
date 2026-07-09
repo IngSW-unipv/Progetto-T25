ec674c7992726c37d01098a7ec2211b03fcec1d9
leahappiah <leah8400@gmail.com>
2026-07-09 20:33:43 +0200

    implementata classe PriceCalculator

--- src/main/java/it/unipv/ingsfw/service/PriceCalculator.java
+++ src/main/java/it/unipv/ingsfw/service/PriceCalculator.java
@@ -1,4 +1,18 @@
 package it.unipv.ingsfw.service;
 
+/**
+ * classe coi prezzi del campo da golf
+ */
 public class PriceCalculator {
+
+    public static double calcolaTotale(int holes, int players, int cart, int golfbag, int trolley) {
+
+        double basePrice = (holes == 9) ? 40.0 : 60.0;
+
+        return (basePrice * players)
+                + (cart * 30.0)
+                + (golfbag * 15.0)
+                + (trolley * 5.0);
+    }
 }
+
