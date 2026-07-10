12d6540ebfaaa46a12e742252e46c798c86e4681
leahappiah <leah8400@gmail.com>
2026-07-10 02:53:43 +0200

    aggiornata classe HomeController relativa all'fxml Home.fxml

--- src/main/java/it/unipv/ingsfw/controller/HomeController.java
+++ src/main/java/it/unipv/ingsfw/controller/HomeController.java
@@ -1,4 +1,49 @@
 package it.unipv.ingsfw.controller;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * classe del controller con la schermata home del portale delle prenotazioni
+ */
 public class HomeController {
+
+    @FXML
+    private void onNuovaPrenotazione(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step1Buche.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onPrices(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/prices.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onSettingsClick(MouseEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/AdminLogin.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
