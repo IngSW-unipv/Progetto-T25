79fda2e702e8b51a22113030ec87c553e467e129
leahappiah <leah8400@gmail.com>
2026-07-13 10:12:45 +0200

    implementata classe corrispondente al sesto step degli extra fxml

--- src/main/java/it/unipv/ingsfw/controller/Step6ExtraController.java
+++ src/main/java/it/unipv/ingsfw/controller/Step6ExtraController.java
@@ -1,4 +1,86 @@
 package it.unipv.ingsfw.controller;
 
+
+
+import it.unipv.ingsfw.exceptions.InvalidReservationException;
+import it.unipv.ingsfw.model.ReservationSession;
+import it.unipv.ingsfw.service.PriceCalculator;
+import it.unipv.ingsfw.service.ReservationValidator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+/**
+ * classe scelta extra
+ */
 public class Step6ExtraController {
+
+    @FXML
+    private Spinner<Integer> spCart;
+
+    @FXML
+    private Spinner<Integer> spGolfbag;
+
+    @FXML
+    private Spinner<Integer> spTrolley;
+
+    private final ReservationValidator validator = new ReservationValidator();
+
+    @FXML
+    public void initialize() {
+        spCart.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 4, 0));
+        spGolfbag.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 4, 0));
+        spTrolley.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 4, 0));
+    }
+
+    @FXML
+    private void onAvanti(ActionEvent e) {
+        try {
+            int numeroGiocatori = ReservationSession.getInstance().getPlayers();
+
+            validator.validaAttrezzatura(numeroGiocatori, spGolfbag.getValue(), spCart.getValue(), spTrolley.getValue());
+            String extraText = spCart.getValue() + " Cart, " + spGolfbag.getValue() + " Sacche, " + spTrolley.getValue() + " Trolley";
+            ReservationSession.getInstance().setExtra(extraText);
+            ReservationSession.getInstance().setCarts(spCart.getValue());
+            ReservationSession.getInstance().setBags(spGolfbag.getValue());
+            ReservationSession.getInstance().setTrolleys(spTrolley.getValue());
+            double total = PriceCalculator.calcolaTotale(
+                    ReservationSession.getInstance().getHoles(),
+                    ReservationSession.getInstance().getPlayers(),
+                    spCart.getValue(),
+                    spGolfbag.getValue(),
+                    spTrolley.getValue()
+            );
+            ReservationSession.getInstance().setTotalPrice(total);
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step7Riepilogo.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+
+        } catch (InvalidReservationException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore Validazione");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onIndietro(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step5Nome.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
