7eb579000b87355b8d542b2ffcdfac36552f492d
leahappiah <leah8400@gmail.com>
2026-07-13 10:09:29 +0200

    classe step4 per l'orario

--- src/main/java/it/unipv/ingsfw/controller/Step4OrarioController.java
+++ src/main/java/it/unipv/ingsfw/controller/Step4OrarioController.java
@@ -1,4 +1,88 @@
 package it.unipv.ingsfw.controller;
 
+
+import it.unipv.ingsfw.model.ReservationSession;
+import it.unipv.ingsfw.service.ReservationValidator;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import java.time.LocalDate;
+
+
+/**
+ * classe scelta orario
+ */
 public class Step4OrarioController {
+
+    @FXML
+    private ComboBox<String> cbTime;
+    private final ReservationValidator validator = new ReservationValidator();
+
+    @FXML
+    public void initialize() {
+        if (cbTime != null) {
+            cbTime.getItems().clear();
+            LocalDate dataSelezionata = ReservationSession.getInstance().getDate();
+            int giocatoriDaPrenotare = ReservationSession.getInstance().getPlayers();
+
+            if (dataSelezionata != null) {
+                for (int ora = 8; ora <= 11; ora++) {
+                    for (int minuto = 0; minuto < 60; minuto += 10) {
+                        String orario = String.format("%02d:%02d", ora, minuto);
+
+                        try {
+                            validator.validaCapacitaTeeTime(dataSelezionata.toString(), orario, giocatoriDaPrenotare);
+
+                            cbTime.getItems().add(orario);
+                        } catch (Exception ex) {
+                        }
+                    }
+                }
+
+                try {
+                    validator.validaCapacitaTeeTime(dataSelezionata.toString(), "12:00", giocatoriDaPrenotare);
+                    cbTime.getItems().add("12:00");
+                } catch (Exception ex) {
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void onAvanti(ActionEvent e) {
+        if (cbTime.getValue() != null) {
+
+            ReservationSession.getInstance().setTime(cbTime.getValue());
+
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/view/step5Nome.fxml"));
+                Scene currentScene = ((Node) e.getSource()).getScene();
+                currentScene.setRoot(root);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void onIndietro(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step3Data.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onTimeSelected(ActionEvent event) {
+        if (cbTime.getValue() != null) {
+            ReservationSession.getInstance().setTime(cbTime.getValue());
+        }
+    }
 }
