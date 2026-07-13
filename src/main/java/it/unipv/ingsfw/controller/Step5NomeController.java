73bef86411d334c8a8e68b66d92a14e287dd2d48
leahappiah <leah8400@gmail.com>
2026-07-13 10:11:26 +0200

    implementata classe step5 scelta nome

--- src/main/java/it/unipv/ingsfw/controller/Step5NomeController.java
+++ src/main/java/it/unipv/ingsfw/controller/Step5NomeController.java
@@ -1,4 +1,49 @@
 package it.unipv.ingsfw.controller;
 
+
+
+import it.unipv.ingsfw.model.ReservationSession;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+
+/**
+ * classe scelta nome
+ */
 public class Step5NomeController {
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    private void onAvanti(ActionEvent e) {
+        if (tfNome.getText() == null || tfNome.getText().trim().isEmpty()) {
+            return;
+        }
+        ReservationSession.getInstance().setCustomerName(tfNome.getText().trim());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step6Extra.fxml"));
+
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onIndietro(ActionEvent e) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/step4Orario.fxml"));
+            Scene currentScene = ((Node) e.getSource()).getScene();
+            currentScene.setRoot(root);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
