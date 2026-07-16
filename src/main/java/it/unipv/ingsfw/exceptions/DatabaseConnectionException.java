e6fbe722f8a379815f0a78cf8b97c8b363ef29ba
leahappiah <leah8400@gmail.com>
2026-07-16 03:52:29 +0200

    aggiunto javadoc classe DatabaseConnectionException.java

--- src/main/java/it/unipv/ingsfw/exceptions/DatabaseConnectionException.java
+++ src/main/java/it/unipv/ingsfw/exceptions/DatabaseConnectionException.java
@@ -1,5 +1,12 @@
 package it.unipv.ingsfw.exceptions;
 
+/**
+ * Eccezione personalizzata per segnalare anomalie o fallimenti
+ * durante la connessione al database.
+ *
+ * @author Leah Appiah
+ * @version 1.0
+ */
 public class DatabaseConnectionException extends RuntimeException {
 
     public DatabaseConnectionException(String message, Throwable cause) {
