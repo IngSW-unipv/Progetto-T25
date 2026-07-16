81d4bb31928896cbf964fa2f6c08cad4a792f919
leahappiah <leah8400@gmail.com>
2026-07-16 03:52:58 +0200

    aggiunto javadoc classe InvalidReservationException.java

--- src/main/java/it/unipv/ingsfw/exceptions/InvalidReservationException.java
+++ src/main/java/it/unipv/ingsfw/exceptions/InvalidReservationException.java
@@ -1,5 +1,12 @@
 package it.unipv.ingsfw.exceptions;
 
+/**
+ * Eccezione personalizzata per segnalare violazioni dei vincoli di business
+ * o di validazione durante la creazione di una prenotazione.
+ *
+ * @author Leah Appiah
+ * @version 1.0
+ */
 public class InvalidReservationException extends Exception {
 
     public InvalidReservationException(String message) {
